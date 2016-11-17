f96d09f2314f741a10364283ed65ca9f15feccb1
Pogos Azaryan <pogosazaryan@nikolay-kulachenko.local>
2016-11-17 13:15:54 +0300

    Initial Commit

---
+++ TripApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripApp
+//
+//  Created by Pogos Azaryan on 17.11.16.
+//  Copyright © 2016 Pogos Azaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
